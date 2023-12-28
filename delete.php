b750ffb468f4b6c426f4366c1610e843abf69ea7
maha-alii <155061843+maha-alii@users.noreply.github.com>
2023-12-28 16:02:26 +0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,18 +1,4 @@
-<?php
-/*include 'connection.php';
-if(isset($_GET['deleteid'])){
-    $id = $_GET['deleteid'];
 
-    $sql = "DELETE FROM notes WHERE id=$id";
-    $result = mysqli_query( $conn, $sql );
-    if($result){
-        //echo'Deleted successfully';
-        header('location:show.php');
-	}
-	else{
-		die( 'Connection failed: ' . htmlspecialchars( $conn->connect_error ) );
-	}
-}*/
 
     include 'connection.php';
 	$sql = $conn->prepare("DELETE  FROM notes WHERE id=?");  

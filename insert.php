7d7152d23e4e07bd479a4874484765b1de93011e
maha-alii <155061843+maha-alii@users.noreply.github.com>
2023-12-28 15:59:37 +0500

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,121 @@
+<?php
+include "connection.php";
+/**if( isset($_POST['save']) ){
+	$title = $_POST['title'];
+	$description = $_POST['description'];
+
+	$sql="INSERT into notes (title, description)
+	VALUES ('$title','$description')";
+	$result = mysqli_query( $conn, $sql );
+	if($result){
+	//	echo "Data inserted Successfully";
+	header('location:show.php');
+	}
+	else{
+		die( 'Connection failed: ' . htmlspecialchars( $conn->connect_error ) );
+	}
+}**/
+
+if ( isset($_POST['save']) ) {	
+	$title = $_POST['title'];
+$description = $_POST['description'];
+	$sql = $conn->prepare("INSERT INTO notes (title,description) VALUES (?, ? )") ;  
+    $sql->bind_param("ss",$title,$description ); 
+	if($sql->execute()) {
+		
+		header('location:show.php');
+	} 
+	
+	
+} 
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Coding Arena</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<style>
+	
+	* {
+			font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Oxygen-Sans", Ubuntu, "Cantarell", "Helvetica Neue", sans-serif;
+		}
+		body {
+			font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Oxygen-Sans", Ubuntu, "Cantarell", "Helvetica Neue", sans-serif;
+			background-color: #f2f2f2;
+			margin: 0;
+			padding: 0;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			height: 100vh;
+		}
+
+		.container {
+			background-color: #fff;
+			padding: 20px;
+			border-radius: 5px;
+			box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+			max-width: 1000px;
+		}
+
+		h1, h2, h3 {
+			color: #333;
+			
+		}
+
+		div {
+			background-color: #f5f5f5;
+			padding: 5px;
+			color: #4f4f4f;
+			border: 1px solid #ddd;
+			border-radius: 3px;
+			display: block;
+		}
+		table, th, td {
+  border:1px solid black;
+}
+
+.btn {
+	background-color: #e7e7e7; color: black;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+		
+		/* Style input fields and buttons */
+		/* Add more styles as needed */
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h1>My Coding Arena</h1>
+		<p>The code shows here:</p>
+		<div>
+			<!-- Add your code below this line -->
+
+			<h2>Notes</h2>
+			<form action="" method="post">
+				<div class="form-group">
+					<label for="title">Title:</label>
+					<input   name="title" id="title" >
+				</div>
+                <div class="form-group">
+					<label for="description">Description:</label>
+					<input   name="description" id="description" >
+                 </div>
+                    <button type="save" class="btn btn-primary" name="save">Save</button>
+				 </form>	
+
+	<div>
+</body>
+</html>			
---
+++ show.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Coding Arena</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<style>
+	
+	* {
+			font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Oxygen-Sans", Ubuntu, "Cantarell", "Helvetica Neue", sans-serif;
+		}
+		body {
+			font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Oxygen-Sans", Ubuntu, "Cantarell", "Helvetica Neue", sans-serif;
+			background-color: #f2f2f2;
+			margin: 0;
+			padding: 0;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			height: 100vh;
+		}
+
+		.container {
+			background-color: #fff;
+			padding: 20px;
+			border-radius: 5px;
+			box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+			max-width: 1000px;
+		}
+
+		h1, h2, h3 {
+			color: #333;
+			
+		}
+
+		div {
+			background-color: #f5f5f5;
+			padding: 5px;
+			color: #4f4f4f;
+			border: 1px solid #ddd;
+			border-radius: 3px;
+			display: block;
+		}
+		table, th, td {
+  border:1px solid black;
+}
+.btn {
+	background-color: #e7e7e7; color: black;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+		
+		/* Style input fields and buttons */
+		/* Add more styles as needed */
+	</style>
+</head>
+<body>
+<div class='container'>
+	
+       <h1>NOTES</h1>
+ <table class="table">
+	<thead>
+		<tr>
+			<th>S.no</th>
+			<th>Title</th>
+			<th>Description</th>
+			<th>options</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		include 'connection.php';
+		$sql = "SELECT * FROM notes";
+        $result = $conn->query($sql);
+		if ($result->num_rows > 0) {		
+			while($row = $result->fetch_assoc()) {
+              $id = $row['id'];
+			  $title = $row['title'];
+			  $description = $row['description'];
+			  echo'
+			  <tr>
+			  <th>'.$id.'</th>
+			  <td>'.$title.'</td>
+			  <td>'.$description.'</td>
+			  <td>
+			  <button class="btn btn-primary my-5"><a href="insert.php">Insert</a></button>
+			  <button class="btn btn-primary"><a href="update.php?updateid='. $id .'">Update</a></button>
+			  <button class="btn btn-danger"><a href="delete.php?deleteid='. $id .'">Delete</a></button>
+			  </td>
+			  </tr>';
+			}
+		}
+		?>
+	</tbody> 
+
+ </table>
+</div>

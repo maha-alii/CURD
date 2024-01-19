3bfdbb7f5cc7387876d0d7d170adfb4d8ab78f5e
adeel-raza <adeelraza77@gmail.com>
2024-01-20 00:22:29 +0500

    update note logic completed

--- show.php
+++ show.php
@@ -8,92 +8,77 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<script>
-			
-			function update_note(id) {
-			var title=jQuery('#note-' + id).find('.note-title').text();
-			var description=jQuery('#note-' + id).find('.note-description ').text();
-			jQuery('#list-notes-wrap').hide();
-			jQuery('#add-note-wrap').show();
-			jQuery('#note-' + id).find('.note-title').val(title);
-			jQuery('#note-' + id).find('.note-description ').val(description);
-
-		}
-		function insert_note(id) {
-		   jQuery('#note_id').val();
-		   var note_id = jQuery('#note_id').val();
-			console.log(note_id)
-			var title = jQuery('#title').val();
-			var description = jQuery('#description').val();
-			if(id){
-				jQuery.ajax({
+		
+	function update_note( id ) {
+		var title = jQuery('#note-' + id).find('.note-title').text();
+		var description = jQuery('#note-' + id).find('.note-description ').text();
+		
+		jQuery('#list-notes-wrap').hide();
+		jQuery('#add-note-wrap').show();
+		jQuery('#note_id').val(id);
+		jQuery('#title').val(title);
+		jQuery('#description').val(description);
+	}
+	
+	function insert_note() {
+		var note_id = jQuery('#note_id').val();
+		var title = jQuery('#title').val();
+		var description = jQuery('#description').val();
+		/* When Updating a Note Via AJAX */
+		if ( note_id ) {
+			jQuery.ajax({
 				url: "update.php",
 				type: "post",
-				data: { 'id':note_id,'title': title , 'description': description },
-				success: function (response) {	
-					jQuery('#list-notes-wrap').show();
-					jQuery('#add-note-wrap').hide();
-					jQuery('#note_id').val();
-					jQuery('.note-title ').val(title);
-					jQuery('.note-description ').val(description);
-			}
-		});
-		}
-		
+				data: {
+					'id': note_id,
+					'title': title,
+					'description': description
+				},
+				success: function(response) {
+					if (!isNaN(response)) {
+						jQuery('#list-notes-wrap').show();
+						jQuery('#add-note-wrap').hide();
+						
+						
+						jQuery('#note-' + note_id).find('.note-title').text(title);
+						jQuery('#note-' + note_id).find('.note-description').text(description);
+					} else {
+						// show error if not deleted
+						console.log(response)
+					}
 
-else{
-	/* When Inserting a Note Via AJAX */
-	jQuery.ajax({
-		url: "insert.php",
-		type: "post",
-		data: { 'title': title , 'description': description },
-		success: function (response) {					
-			// You will get response from your PHP page (what you echo or print)
-			if( !isNaN(response)) {
-				var note_id = response;
-				jQuery('#list-notes-body' ).append(
-					'<tr id=note-' + note_id + '>\
+				}
+		  });
+		} else {
+			/* When Inserting a Note Via AJAX */
+			jQuery.ajax({
+				url: "insert.php",
+				type: "post",
+				data: {
+					'title': title,
+					'description': description
+				},
+				success: function(response) {
+					// You will get response from your PHP page (what you echo or print)
+					if (!isNaN(response)) {
+						var note_id = response;
+						jQuery('#list-notes-body').append(
+							'<tr id=note-' + note_id + '>\
 						<th>' + note_id + '</th>\
-						<td class="note-title">' + title +'</td>\
+						<td class="note-title">' + title + '</td>\
 						<td class="note-description">' + description + '</td>\
 						<td> \
-							<a onclick="update_note('+note_id+')">\
+							<a onclick="update_note(' + note_id + ')">\
 								<button class="btn btn-lg btn-primary">Update</button>\
 							</a>\
-							<a onclick="delete_note('+note_id+')">\
+							<a onclick="delete_note(' + note_id + ')">\
 								<button class="btn btn-lg btn-danger">Delete</button>\
 							</a>\
 						</td>\
 					</tr>'
-				);
-				jQuery('#list-notes-wrap').show();
-				jQuery('#add-note-wrap').hide();
-			} else {
-				// show error if not deleted
-				console.log(response)
-			}
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			console.log("error in AJAX request: " + errorThrown);
-		}
-	});	
-}
-}
-		
-		function show_note() {
-			jQuery('#list-notes-wrap').hide();
-			jQuery('#add-note-wrap').show();
-		}
-		
-		function delete_note( id ) {
-			jQuery.ajax({
-				url: "delete.php?delete_id=" + id,
-				type: "get",
-				//type: "post",
-				//data: {'ad': 123, 'asasasa': 'asasas'},
-				success: function (response) {					
-					// You will get response from your PHP page (what you echo or print)
-					if( response == 1) {
-						jQuery('#note-' + id ).remove();
+						);
+						jQuery('#list-notes-wrap').show();
+						jQuery('#add-note-wrap').hide();
 					} else {
 						// show error if not deleted
 						console.log(response)
@@ -102,10 +87,43 @@ else{
 				error: function(jqXHR, textStatus, errorThrown) {
 					console.log("error in AJAX request: " + errorThrown);
 				}
-			});	
+			});
 		}
+	}
+	
+	function show_list_note() {
+		jQuery('#list-notes-wrap').show();
+		jQuery('#add-note-wrap').hide();
+	}
+	
+	function show_insert_note() {
+		// Empty the note id, title and desc before insert form display
+		jQuery('#title').val('');
+		jQuery('#description').val('');
+		jQuery('#note_id').val('');
+		
+		jQuery('#list-notes-wrap').hide();
+		jQuery('#add-note-wrap').show();
+	}
 
-
+	function delete_note(id) {
+		jQuery.ajax({
+			url: "delete.php?delete_id=" + id,
+			type: "get",
+			success: function(response) {
+				// You will get response from your PHP page (what you echo or print)
+				if (response == 1) {
+					jQuery('#note-' + id).remove();
+				} else {
+					// show error if not deleted
+					console.log(response)
+				}
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("error in AJAX request: " + errorThrown);
+			}
+		});
+	}
 	</script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<style>
@@ -156,15 +174,15 @@ else{
 </head>
 <body>
 <div class='container'>
-	
+
 	<h1>NOTES</h1>
-	<a onclick="show_note()">
+	<a onclick="show_insert_note()">
 		<button class="btn btn-lg btn-primary my-5  float-right-top">Insert</button>
-	</a> 
-	<a href='show.php'>
+	</a>
+	<a onclick="show_list_note()">
 		<button class="btn btn-lg btn-primary my-5  float-right-top ">List of notes</button>
-	</a> 
-				
+	</a>
+
 	<table id="list-notes-wrap" class="table">
 		<thead>
 			<tr>
@@ -185,42 +203,42 @@ else{
 					$title       = $row['title'];
 					$description = $row['description'];
 					?>
-					<tr id=note-<?php echo htmlspecialchars( $id ); ?>>
-						<th class="id"><?php echo htmlspecialchars( $id ); ?></th>
-						<td class="note-title"><?php echo htmlspecialchars( $title ); ?></td>
-						<td class="note-description"><?php echo htmlspecialchars( $description ); ?></td>
-						<td>
-							<a onclick="update_note(<?php echo htmlspecialchars( $id ); ?>)">
-								<button class="btn btn-lg btn-primary">Update</button>
-							</a>
-							<a onclick="delete_note(<?php echo htmlspecialchars( $id ); ?>)">
-								<button class="btn btn-lg btn-danger">Delete</button>
-							</a>
-						</td>
-					</tr>
+			<tr id=note-<?php echo htmlspecialchars( $id ); ?>>
+				<th class="id"><?php echo htmlspecialchars( $id ); ?></th>
+				<td class="note-title"><?php echo htmlspecialchars( $title ); ?></td>
+				<td class="note-description"><?php echo htmlspecialchars( $description ); ?></td>
+				<td>
+					<a onclick="update_note(<?php echo htmlspecialchars( $id ); ?>)">
+						<button class="btn btn-lg btn-primary">Update</button>
+					</a>
+					<a onclick="delete_note(<?php echo htmlspecialchars( $id ); ?>)">
+						<button class="btn btn-lg btn-danger">Delete</button>
+					</a>
+				</td>
+			</tr>
 					<?php
 				}
 			}
 			?>
 		</tbody>
 	</table>
-	
+
 	<!-- HTML structre for Add Note -->
 	<div id="add-note-wrap">
-	<h2>Add Notes</h2>			
-	<form class="form" method="post">
-		<div class="form-group">
-			<label for="title">Title:</label>
-			<input class="form-control " name="title" id="title" >
-		</div>
-		<div class="form-group">
-			<label for="description">Description:</label>
-			<input class="form-control " name="description" id="description">
-		</div>
-		<input class="form-control" type="hidden" name="note_id" id="note_id" value="">
-		<button onclick="insert_note()" type="button" class="btn btn-primary" name="save">Save</button>
-	</form>
+		<h2>Add Notes</h2>
+		<form class="form" method="post">
+			<div class="form-group">
+				<label for="title">Title:</label>
+				<input class="form-control" name="title" id="title" required>
+			</div>
+			<div class="form-group">
+				<label for="description">Description:</label>
+				<input class="form-control" name="description" id="description">
+			</div>
+			<input class="form-control" type="hidden" name="note_id" id="note_id" value="">
+			<button onclick="insert_note()" type="button" class="btn btn-primary" name="save">Save</button>
+		</form>
+	</div>
 </div>
-</div>	
 </body>
 </html>
--- update.php
+++ update.php
@@ -1,37 +1,35 @@
 <?php
 // No note variable found, or invalid delete_id, so exit
-if ( ! isset( $_GET['update_id'] ) || ! is_numeric( $_GET['update_id'] ) ) {
+if ( ! isset( $_POST['id'] ) || ! is_numeric( $_POST['id'] ) ) {
 	die();
 }
 // Added databse connection file
 require 'connection.php';
 
 // Update note on user sumbit
-if ( isset( $_POST['save'] ) ) {
+if ( isset( $_POST['title'] ) ) {
 	// Prepare the update statement with a placeholder for the parameter
 	$sql         = $conn->prepare( 'UPDATE notes SET  title=?, description=? WHERE id=? ' );
+	$note_id     = $_POST['id'];
 	$title       = $_POST['title'];
 	$description = $_POST['description'];
+	
 	// Bind the parameter to the statement
+	$sql->bind_param( 'ssi', $title, $description, $note_id );
 	
 		// Execute the statement
-		if ( $sql->execute() ) {
-			$sql->bind_param( 'ssi', $title, $description, $_GET['update_id'] );
-	
-			// Check the number of affected rows
-			if ( $sql->affected_rows > 0 ) {
-				echo htmlspecialchars( $sql->insert_id ); // Success indicator
-			} else {
-				echo 'No rows affected. The record may not exist.';
-			}
+	if ( $sql->execute() ) {
+		// Check the number of affected rows
+		if ( $sql->affected_rows > 0 ) {
+			echo htmlspecialchars( $note_id ); // Success indicator
 		} else {
-			echo 'Error executing statement: ' . htmlspecialchars( $sql->error );
+			echo 'No rows affected. The record may not exist.';
 		}
 	} else {
-		echo 'Error preparing statement: ' . htmlspecialchars( $conn->error );
+		echo 'Error executing statement: ' . htmlspecialchars( $sql->error );
 	}
+} else {
+	echo 'Error preparing statement: ' . htmlspecialchars( $conn->error );
+}
 
 die();
-
-
-?>
